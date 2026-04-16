70e6cd2f2f5ef8b3635acdbeed0c73766e5b31b8
Sagar Gupta <sg85207@gmail.com>
2026-04-16 08:54:04 +0530

    chore: remove obsolete CHANGELOG.md file

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import UserInDB, UserPublic, UserStats
-from app.models.room import RoomCreate, RoomInDB, RoomPublic, RoomSettings, DIFFICULTY_TIME_LIMITS
-from app.models.problem import ProblemInDB, ProblemPublic, TestCase
-from app.models.submission import SubmissionCreate, SubmissionInDB
-from app.models.match_result import MatchResultInDB, PlayerRanking
--- backend/app/ws/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.ws.manager import manager
